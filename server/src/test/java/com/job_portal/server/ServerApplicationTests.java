51d8ffcdc05e46bc0b10c3d61757276acce8b80b
Rustam Rakhmatullov <rahmatullovri@gmail.com>
2024-09-25 12:20:49 +0400

    server was added

---
+++ server/src/test/java/com/job_portal/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job_portal.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
